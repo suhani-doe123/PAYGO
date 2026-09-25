805352f64e28758bf1cf1f2fcf8d7382a5b2df31
Mohammed Suhaib <raksssuhaib@gmail.com>
2026-09-26 00:50:45 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,96 @@
+const CACHE_NAME = "payko-v6";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  
+  event.waitUntil(
+    
+    caches.open(CACHE_NAME)
+    .then(cache => cache.addAll(ASSETS))
+    .then(() => self.skipWaiting())
+    
+  );
+  
+});
+
+
+self.addEventListener("activate", event => {
+  
+  event.waitUntil(
+    
+    caches.keys().then(keys => {
+      
+      return Promise.all(
+        
+        keys
+        .filter(key => key !== CACHE_NAME)
+        .map(key => caches.delete(key))
+        
+      );
+      
+    }).then(() => self.clients.claim())
+    
+  );
+  
+});
+
+
+self.addEventListener("fetch", event => {
+  
+  if (event.request.method !== "GET") return;
+  
+  const url = new URL(event.request.url);
+  
+  if (url.origin !== self.location.origin) return;
+  
+  event.respondWith(
+    
+    caches.match(event.request)
+    .then(cached => {
+      
+      if (cached) return cached;
+      
+      return fetch(event.request)
+        .then(response => {
+          
+          if (
+            !response ||
+            response.status !== 200
+          ) {
+            return response;
+          }
+          
+          const copy = response.clone();
+          
+          caches.open(CACHE_NAME)
+            .then(cache => {
+              cache.put(event.request, copy);
+            });
+          
+          return response;
+          
+        })
+        .catch(() => {
+          
+          if (event.request.mode === "navigate") {
+            
+            return caches.match("./index.html");
+            
+          }
+          
+        });
+      
+    })
+    
+  );
+  
+});
